f6f81c5ab12f0d144805a23df68e577c534c2211
Cliff <tzcliff@outlook.com>
2019-11-27 21:11:08 -0500

    course list view

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,9 +1,10 @@
+import views.CourseListView;
 import views.LoginView;
 
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hi");
-        LoginView window = new LoginView();
+        CourseListView window = new CourseListView();
     }
 }
--- src/src/views/LoginView.java
+++ src/src/views/LoginView.java
@@ -11,9 +11,34 @@ public class LoginView {
 
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(500, 600);
+        frame.setSize(600, 450);
+        JLabel welcomeLabel = new JLabel("Welcome to BU Grading System", SwingConstants.CENTER);
+        JLabel userIDLabel = new JLabel("User ID: ", SwingConstants.CENTER);
+        JLabel pwdLabel = new JLabel("Password: ", SwingConstants.CENTER);
+        JTextField idText =new JTextField("User ID");
+        JPasswordField pwdText =new JPasswordField();
+        pwdText.setEchoChar('*');
+        JButton signInButton = new JButton("Sign In");
+        JButton cancelButton = new JButton("cancel");
+        Panel infoPanel = new Panel();
+        Panel buttonPanel = new Panel();
 
-        frame.setLayout(new GridLayout(4, 1, 0, 20));
+
+        infoPanel.setLayout(new GridLayout(2, 2, 30,40));
+        infoPanel.add(userIDLabel);
+        infoPanel.add(idText);
+        infoPanel.add(pwdLabel);
+        infoPanel.add(pwdText);
+
+        buttonPanel.add(signInButton);
+        buttonPanel.add(cancelButton);
+        buttonPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10));
+        frame.add(welcomeLabel);
+        frame.add(infoPanel);
+        frame.add(buttonPanel);
+
+
+        frame.setLayout(new GridLayout(3, 1, 20, 50));
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
         frame.setVisible(true);
