6b6cf507848c5058ef8ae9d7b45a1a174b794c93
ThanasisFilippidis <sdi1400215@di.uoa.gr>
2019-12-06 11:51:34 -0500

    Added new addNewCourse method. It will be used when creating course based on a template.

--- src/src/models/GradingSystem.java
+++ src/src/models/GradingSystem.java
@@ -22,11 +22,24 @@ public class GradingSystem extends ProfessorsTool{
         return courseTemplates;
     }
 
+    // Creates a new course NOT based in any previous one
     public void addNewCourse(String name, String semester, String year, ArrayList<Student> students){
         CourseSection courseSection = new CourseSection(0,"First section", students);
         ArrayList<CourseSection> courseSections = new ArrayList<CourseSection>();
         courseSections.add(courseSection);
         CourseTemplate newCourseTemplate = new CourseTemplate(0, name, semester, year);
+        Course newCourse = new Course(0, name, semester, year, courseSections, newCourseTemplate);
+
+        courseTemplates.add(newCourseTemplate);
+        activeCourses.add(newCourse);
+    }
+
+    // Creates a new course based on the template courseTemplate
+    public void addNewCourse(String name, String semester, String year, ArrayList<Student> students, CourseTemplate courseTemplate){
+        CourseSection courseSection = new CourseSection(0,"First section", students);
+        ArrayList<CourseSection> courseSections = new ArrayList<CourseSection>();
+        courseSections.add(courseSection);
+        CourseTemplate newCourseTemplate = new CourseTemplate(0, name, semester, year, courseTemplate);
         Course newCourse = new Course(0, name, semester, year, courseSections, newCourseTemplate);
 
         courseTemplates.add(newCourseTemplate);
