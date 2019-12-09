25bde1b4f17d511c36a2be2ac82355f8b107d83e
ThanasisFilippidis <sdi1400215@di.uoa.gr>
2019-12-08 19:27:08 -0500

    Finalized the last nights SQL script changes.

--- DatabaseScript/DataModel.sql
+++ DatabaseScript/DataModel.sql
@@ -40,10 +40,7 @@ CREATE TABLE `Professor` (
 CREATE TABLE `Enrollment` (
                               `id` int PRIMARY KEY AUTO_INCREMENT,
                               `studentId` int,
-                              `courseId` int,
-    -- `isActive` bit, Maybe those two -> isWithdrawn?
-    -- `status` int,
-    -- `comment` varchar(255),
+                              `isWithdrawn` boolean,
                               `courseSectionId` int
 );
 
@@ -55,25 +52,22 @@ CREATE TABLE `Student` (
                            `last_name` varchar(255),
                            `email` varchar(255),
                            `buid` varchar(255)
-    -- `comment` varchar(255),
-    -- `created_at` timestamp
 );
 
 CREATE TABLE `TemplateCourse` (
                                   `id` int PRIMARY KEY AUTO_INCREMENT,
                                   `name` varchar(255),
-                                  `code` varchar(255),
-                                  `created_at` timestamp
+                                  `year` varchar(255),
+                                  `semester` varchar(255),
 );
 
 CREATE TABLE `Course` (
                           `id` int PRIMARY KEY AUTO_INCREMENT,
-    -- `code` varchar(255),
-    -- Add maybe year - semester?
+                          `templateCourseId` int,
+                          `year` varchar(255),
+                          `semester` varchar(255),
                           `name` varchar(255),
-    -- `created_at` timestamp,
                           `professorId` int,
-                          `isDeleted` boolean
 );
 CREATE TABLE `CourseSection` (
                                  `id` int PRIMARY KEY AUTO_INCREMENT,
@@ -81,50 +75,24 @@ CREATE TABLE `CourseSection` (
                                  `courseId` int
 );
 
--- Maybe just use Task?
-CREATE TABLE `TemplateTask` (
-                                `id` int PRIMARY KEY AUTO_INCREMENT,
-                                `name` varchar(255),
-                                `courseId` int,
-                                `weight` double,
-                                `created_at` timestamp
-);
-
 CREATE TABLE `Task` (
                         `id` int PRIMARY KEY AUTO_INCREMENT,
                         `name` varchar(255),
-                        `courseId` int,
+                        `templateCourseId` int,
                         `weight` double,
-    -- `created_at` timestamp,
                         `isDeleted` boolean
 );
 
--- Maybe just use SubTask?
-CREATE TABLE `TemplateSubTask` (
-                                   `taskId` int PRIMARY KEY AUTO_INCREMENT,
-                                   `weight` double,
-                                   `name` varchar(255),
-                                   `maxScore` int,
-                                   `scoreType` int,
-                                   `created_at` timestamp,
-                                   `releasedDate` datetime,
-                                   `dueDate` datetime,
-                                   `isGroupWork` bit
-);
 CREATE TABLE `SubTask` (
                            `id` int PRIMARY KEY AUTO_INCREMENT,
                            `taskId` int,
                            `weight` double,
                            `name` varchar(255),
                            `totalPointsAvailable` float,
-    -- `scoreType` int,
-    -- `creationDate` timestamp,
                            `releasedDate` datetime,
                            `dueDate` datetime,
                            `groupProject` bit,
-                           `isDeleted` boolean,
                            `maxAvailableBonusPoints` float
-    -- Changed the bonuses implementation TODO: Discuss about it
 );
 
 CREATE TABLE `Grade` (
@@ -134,8 +102,6 @@ CREATE TABLE `Grade` (
                          `absolutePointsScored` float ,
                          `bonusPoints` float ,
                          `comment` varchar(255),
-    -- `created_at` timestamp,
-                         `isDeleted` boolean
 );
 
 
@@ -145,7 +111,7 @@ CREATE TABLE `Grade` (
 
 ALTER TABLE `Grade` ADD FOREIGN KEY (`enrollmentId`) REFERENCES `Enrollment` (`id`);
 
-ALTER TABLE `Task` ADD FOREIGN KEY (`courseId`) REFERENCES `Course` (`id`);
+ALTER TABLE `Task` ADD FOREIGN KEY (`templateCourseId`) REFERENCES `TemplateCourse` (`id`);
 
 ALTER TABLE `SubTask` ADD FOREIGN KEY (`taskId`) REFERENCES `Task` (`id`);
 
@@ -157,10 +123,8 @@ ALTER TABLE `Credential` ADD FOREIGN KEY (`id`) REFERENCES `Professor` (`id`);
 
 ALTER TABLE `Course` ADD FOREIGN KEY (`professorId`) REFERENCES `Professor` (`id`);
 
+ALTER TABLE `Course` ADD FOREIGN KEY (`templateCourseId`) REFERENCES `TemplateCourse` (`id`);
+
 ALTER TABLE `CourseSection` ADD FOREIGN KEY (`courseId`) REFERENCES `Course` (`id`);
 
 ALTER TABLE `Enrollment` ADD FOREIGN KEY (`courseSectionId`) REFERENCES `CourseSection` (`id`);
-
-ALTER TABLE `TemplateTask` ADD FOREIGN KEY (`courseId`) REFERENCES `TemplateCourse` (`id`);
-
-ALTER TABLE `TemplateSubTask` ADD FOREIGN KEY (`taskId`) REFERENCES `TemplateTask` (`id`);
