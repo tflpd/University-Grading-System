9b9330ea864b3828756c5678eb25f7541e3c048c
ThanasisFilippidis <sdi1400215@di.uoa.gr>
2019-12-12 17:42:26 -0500

    merging commit2

--- src/src/models/Task.java
+++ src/src/models/Task.java
@@ -48,6 +48,7 @@ public class Task {
 
     public void addNewSubTask(ArrayList<Student> students, String name, LocalDateTime creationDate, LocalDateTime dateDue, Float totalPointsAvailable, Float weightInParentTask, Float bonusPoints, String otherComments, boolean groupProject){
         subTasks.add(new SubTask(0, students, name, creationDate,  dateDue.toString(), totalPointsAvailable, weightInParentTask, bonusPoints, otherComments, groupProject));
+    }
 
    public void addNewSubTask(ArrayList<Student> students, String name, LocalDateTime creationDate, String dateDue, Float totalPointsAvailable, Float weightInParentTask, Float bonusPoints, String otherComments, boolean groupProject){
         subTasks.add(new SubTask(0, students, name, creationDate, dateDue, totalPointsAvailable, weightInParentTask, bonusPoints, otherComments, groupProject));
